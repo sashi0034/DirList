ae7bf0e7664ba1e11cc357d14234de01d85cfdfc
sashi0034 <satoshi3104.nishi@gmail.com>
2022-09-03 17:31:23 +0900

    DirLineElementの追加

---
+++ DirPath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirList
+{
+    public struct DirPath
+    {
+        private string _path;
+        public string Path => _path;
+
+        public DirPath(string path)
+        {
+            _path = path;
+        }
+
+
+    }
+}
---
+++ Views/DirLineElement.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DirList.Views
+{
+    /// <summary>
+    /// DirLineElement.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DirLineElement : UserControl
+    {
+        public DirLineElement()
+        {
+            InitializeComponent();
+
+            Dir = new DirPath("C:\\dev");
+        }
+
+        public DirPath Dir
+        {
+            get
+            {
+                return new DirPath(buttonDir.Content as string);
+            }
+            set
+            {
+                buttonDir.Content = value.Path;
+            }
+        } 
+
+    }
+}
